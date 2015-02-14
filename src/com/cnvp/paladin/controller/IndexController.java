0f19f0c2fb60f41ec7456ef6399c7b0f71866c7c
michael.z <mail@zhangxiao.org>
2015-02-14 12:29:34 +0800

    完成用户修改密码功能

--- src/com/cnvp/paladin/controller/IndexController.java
+++ src/com/cnvp/paladin/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.cnvp.paladin.controller;
 import java.util.List;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 
 import com.cnvp.paladin.core.BaseController;
 import com.cnvp.paladin.kit.PropertyKit;
@@ -19,6 +18,40 @@ public class IndexController extends BaseController {
 		setAttr("tree", tree);
 	}	
 	public void welcome() {
+	}
+	public void password(){
+		if("POST".equals(getRequest().getMethod())){
+			SysUser currentUser  = (SysUser) SecurityUtils.getSubject().getPrincipal();
+			currentUser = SysUser.dao.findById(currentUser.get("id"));
+			String old = getPara("old");
+			String new1 = getPara("new1");
+			String new2 = getPara("new2");
+			if (StringKit.isBlank(old))
+				alertAndGoback("原始密码不能为空");
+			else if (!currentUser.getStr("password").equals(EncryptionKit.md5Encrypt(old))) 
+				alertAndGoback("原始密码错误");			
+			else if (!new1.equals(new2))
+				alertAndGoback("新密码两次输入不一致");
+			else{
+				SysUser.dao
+				.set("id", currentUser.get("id"))
+				.set("password", EncryptionKit.md5Encrypt(new1))
+				.update();
+				alertAndGoback("密码修改成功");
+			}
+				
+//			else{
+//				String psw = model.getStr("password");
+//				model.set("password",EncryptionKit.md5Encrypt(psw));				
+//			}
+//			model
+//			.set("id", getParaToInt())
+//			.set("update_time", System.currentTimeMillis())
+//			.set("update_user_id", 1);
+//			if(model.update())
+//				redirect(getControllerKey());
+//			return;
+		}
 	}
 	public void profile() {
 		SysUser currentUser  = (SysUser) SecurityUtils.getSubject().getPrincipal();

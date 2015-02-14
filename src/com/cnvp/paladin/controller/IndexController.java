8ac4199068bc0dd0af2ff6312a52bedf8756d4b3
michael.z <mail@zhangxiao.org>
2015-02-14 12:06:10 +0800

    1、form的代码生成页面增加enum类型规则 2、完善个人资料的修改

--- src/com/cnvp/paladin/controller/IndexController.java
+++ src/com/cnvp/paladin/controller/IndexController.java
@@ -7,8 +7,11 @@ import org.apache.shiro.subject.Subject;
 
 import com.cnvp.paladin.core.BaseController;
 import com.cnvp.paladin.kit.PropertyKit;
+import com.cnvp.paladin.kit.StringKit;
 import com.cnvp.paladin.model.SysNav;
+import com.cnvp.paladin.model.SysUser;
 import com.cnvp.paladin.service.NavService;
+import com.jfinal.kit.EncryptionKit;
 
 public class IndexController extends BaseController {
 	public void index() {
@@ -18,8 +21,13 @@ public class IndexController extends BaseController {
 	public void welcome() {
 	}
 	public void profile() {
-		Subject currentUser  = SecurityUtils.getSubject(); 
-		renderText((String) currentUser.getPrincipal());
+		SysUser currentUser  = (SysUser) SecurityUtils.getSubject().getPrincipal();
+		if("POST".equals(getRequest().getMethod())){
+			if(getModel(SysUser.class,"user").set("id", currentUser.getInt("id")).update())
+				redirect("/profile");
+				return;
+		}
+		setAttr("data", SysUser.dao.findById(currentUser.getInt("id")));
 	}
 	public void closed() {
 		setAttr("app_remark", PropertyKit.get("app_remark"));

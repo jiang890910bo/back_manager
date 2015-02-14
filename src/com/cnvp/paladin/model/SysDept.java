3831fcc8c3423d3dca2c133fc25e3b86e211a30f
michael.z <mail@zhangxiao.org>
2015-02-14 12:43:10 +0800

    完善模型生成，并生成SysDept

--- src/com/cnvp/paladin/model/SysDept.java
+++ src/com/cnvp/paladin/model/SysDept.java
@@ -4,9 +4,9 @@ import com.cnvp.paladin.core.BaseModel;
 import com.jfinal.plugin.activerecord.Page;
 
 @SuppressWarnings("serial")
-public class SysUser extends BaseModel<SysUser> {
-	public static final SysUser dao = new SysUser();
-	public Page<SysUser> paginate(int pageNumber, int pageSize) {
+public class SysDept extends BaseModel<SysDept> {
+	public static final SysDept dao = new SysDept();
+	public Page<SysDept> paginate(int pageNumber, int pageSize) {
 		return paginate(pageNumber, pageSize, "select *", "from "+getTableName()+" order by id asc");
 	}
 }
